d518f6121389156d17894d501ffcd1fc222137f3
Huzaifa Arif <huzaifarif@gmail.com>
2020-06-04 13:12:54 +0530

    Update personal info and contact links

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://huzaifarif.dev',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Huzaifa Arif',
+  subtitle: 'A place where I speak my mind, jot down stuff that I find interesting, and the ocassional notes to self 😃.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
+  postsPerPage: 5,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
@@ -25,19 +25,19 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Huzaifa Arif',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Full stack developer, DevOps engineer, novice blogger, tech and gadget enthusiast.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 'contact@huzaifarif.dev',
+      facebook: '',
+      telegram: '',
+      twitter: 'arif_huzaifa',
+      github: 'huzaifarif',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'huzaifarif',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
